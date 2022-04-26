850348caf085464b897085b151879407d43ce9f8
Victor e Esthefany <victor.teka12@gmail.com>
2022-04-25 22:14:42 -0300

    primerio commit

---
+++ testeform.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
+
+echo $nome;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $telefone;
+echo "<br>";
+echo $assunto;
+echo "<br>";
+echo $mensagem;
+echo "<br>";
+?>
+
+
